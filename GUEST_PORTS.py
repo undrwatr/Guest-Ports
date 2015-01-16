b8362dd982c2ed9ad8d74ed5f13a8979b85a860e
Joe Jenkins <joe@breathe-underwater.com>
2015-01-16 11:40:25 -0800

    Change to the enable command to use cred.password

--- GUEST_PORTS.py
+++ GUEST_PORTS.py
@@ -16,15 +16,16 @@ def disable_paging(command="terminal length 0\n", delay=1):
 	remote_conn.send(command)
 	time.sleep(delay)
 
-enablepass=cred.password
 # defines enable  and conf t for logging into the router
-def enable(command1="en\n", command2="cred.password\n", command3="conf t\n",delay=1):
+def enable(command1="en\n", command2=cred.password, command3="\n", command4="conf t\n", delay=1):
 	remote_conn.send("\n")
 	remote_conn.send(command1)
 	time.sleep(delay)
 	remote_conn.send(command2)
 	time.sleep(delay)
 	remote_conn.send(command3)
+	time.sleep(delay)
+	remote_conn.send(command4)
 	time.sleep(delay)
 
 # open a file to dump the data in for the sh ip int brief
